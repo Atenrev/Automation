a2f86859b09790c057269715aeee63934338adc4
atenrev <atenrev@gmail.com>
2017-10-20 22:23:17 +0200

    Changed few things.

--- Java/javaMenuGenerator.py
+++ Java/javaMenuGenerator.py
@@ -9,16 +9,20 @@ else:
 
 options = []
 print ("---Options---:")
-inp = "y"
-while len(inp) != 0:
+inp = ""
+while len(inp) == 0:
     print ("Write the option name:")
-    options.append("\t\t\tcase {0}: {1}(); break;".format(len(options)+1,input()))
-    print ("Wanna add another case? (blank = no, something = yes:")
+    options.append("\t\t\tcase {0}: {1}(); break;".format(len(options),input()))
+    print ("Wanna add another case? (blank = yes, something = no:")
     inp = input()
+
+print ("Write the text (with the format you want) you want to appear on the menu:")
+text = input()
 
 menu = [
     "public " + static + "void " + name + "() {",
     "\tint op",
+    "\tSystem.out.println('{0}');".format(text),
     "\tdo {",
     "\t\top = USERINPUT",
     "\t\tswitch(op) {",
